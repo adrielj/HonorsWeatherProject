23c3f642ad51e301f51b506de1aad65e31b9e22d
adrielj <adrieljuarez2019@gmail.com>
2020-02-23 19:32:46 -0500

    Project 1 of the Weather Project

---
+++ src/Project1.java
@@ -0,0 +1,18 @@
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String stationName = "Monmouth University";
+		double temp = 60;
+		int windSpeed = 10;
+		String windDirection = "NW";
+		
+		System.out.println("The " +stationName+ " Weather Station");
+		System.out.println("Temperature: " +temp+ "°F");	
+		System.out.println("Wind Speed/Direction: " +windSpeed+ " mph " +windDirection);
+				
+	}
+
+}
